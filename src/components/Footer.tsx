96daafaa3871ae42e747a27779b01bfbb4e7bec3
scrpmangatest <miciotucuk@gmail.com>
2026-08-24 22:53:29 +0700

    Histats div counter di footer + param 600,110,30

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,6 +7,9 @@ export default function Footer() {
   return (
     <footer className="site-footer">
       <div className="container">
+        <div style={{ display: "flex", justifyContent: "center", padding: "var(--s3) 0 var(--s2)" }}>
+          <div id="histats_counter"></div>
+        </div>
         <div className="footer-bottom" style={{ borderTop: "none", padding: "var(--s4) 0" }}>
           <span>
             © {year} {SITE.name}. Chord untuk keperluan belajar dan latihan.
