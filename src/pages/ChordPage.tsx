f496500e857443e772b069c4ebd5ca434b3d21d5
scrpmangatest <miciotucuk@gmail.com>
2026-09-02 20:31:53 +0700

    style: samakan ukuran diagram chord family dengan diagram utama

--- src/pages/ChordPage.tsx
+++ src/pages/ChordPage.tsx
@@ -270,7 +270,7 @@ export default function ChordPage({ song }: { song: Song }) {
               <div style={{ display: "flex", gap: 10, overflowX: "auto", marginTop: "var(--s2)", paddingBottom: "var(--s1)" }}>
                 {family.chords.map((chord) => (
                   <div key={chord} style={{ flex: "0 0 auto", textAlign: "center" }}>
-                    <ChordDiagram chord={chord} size={52} />
+                    <ChordDiagram chord={chord} size={60} />
                     <span className="caption" style={{ fontSize: 9, fontWeight: 600, display: "block", marginTop: 2 }}>{chord}</span>
                   </div>
                 ))}
